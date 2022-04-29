aa635e6baf489a707bdc0d01ea0e7563705e21d5
ddiniz <ddiniz@student.42sp.org.br>
2022-04-29 16:45:11 -0300

    Delivery of the project.

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 09:21:24 by ddiniz            #+#    #+#             */
-/*   Updated: 2022/04/29 15:52:47 by ddiniz           ###   ########.fr       */
+/*   Updated: 2022/04/29 16:43:40 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	long	x;
+	unsigned int	aux;
 
-	x = n;
+	aux = n;
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		x = (x * (-1));
+		aux = (aux * (-1));
 	}
-	if (x >= 10)
-		ft_putnbr_fd((x / 10), fd);
-	ft_putchar_fd(((x % 10) + 48), fd);
+	if (aux >= 10)
+		ft_putnbr_fd((aux / 10), fd);
+	ft_putchar_fd(((aux % 10) + 48), fd);
 }
